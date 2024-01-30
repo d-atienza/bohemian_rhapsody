fc90f1aa8fdc5fff99e4c1079794fd3d99dbeccf
d-atienza <153103146+d-atienza@users.noreply.github.com>
2024-01-30 12:08:47 +0000

    stuff to commit

--- app.js
+++ app.js
@@ -9,18 +9,47 @@ app.listen(3000);
 //searches for files with ejs in it in 'views' folder
 app.set("view engine", "ejs");
 
+// first part
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// middle part
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
+// end part
 app.get("/finale", (req, res) => {
   res.render("finale");
 });
 
+// members
+app.get("/members", (req, res) => {
+  const members = [
+    { name: "Freddie Mercury", instrument: "Vocalist" },
+    { name: "Brian May", instrument: "Guitarist" },
+    { name: "Roger Taylor", instrument: "Bass Guitarist" },
+    { name: "John Deacon", instrument: "Drummer" },
+  ];
+
+  res.render("members", { members });
+});
+
+// other songs
+app.get("/songs", (req, res) => {
+  const songs = [
+    "We Will Rock You",
+    "Dont Stop Me Now",
+    "We Are The Champions",
+    "Bicycle Race",
+    "Killer Queen",
+  ];
+
+  res.render("songs", { songs });
+});
+
+// lucky draw
 app.get("/diceroll", (req, res) => {
   const dice = [1, 2, 3, 4, 5, 6];
 
@@ -31,3 +60,28 @@ function roll(arr) {
   const index = Math.floor(Math.random() * arr.length);
   return "... and you have rolled a " + arr[index];
 }
+
+//music query
+//////////////////////////////
+const { Pool } = require("pg");
+const pool = makeDBConnectionPool("QueenSongsDB");
+
+function makeDBConnectionPool(dbName) {
+  //Understanding the details is not important here.
+  return new Pool({
+    database: dbName,
+  });
+}
+
+async function dbDisplay() {
+  const dbResult = await pool.query("select * from queen_songs");
+  const songArray = dbResult.rows;
+  return songArray;
+}
+//////////////////////////////
+
+app.get("/songinfo", async (req, res) => {
+  let result = await dbDisplay();
+
+  res.render("songinfo", { songinfo: result });
+});
